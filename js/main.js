03c7e7d28e3ed14d0f8afa798923551e131744a0
Arif0004 <arifmahmudaj6@gmail.com>
2022-06-14 12:19:56 +0600

    asynchronous

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//asyncronous: setTimeout, setInterval
+console.log('Line 1');
+setTimeout(function(){    //setTimeout take 2 parameters
+    console.log('Line 2');
+},2000);
+
+console.log('Line 3')
